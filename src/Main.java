a8e1c4246d97b23ddb9f74064daad1302f47dec5
jul <julia_sky@mail.ru>
2022-12-27 20:29:31 +0300

    Решение задачи, билет 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
+ipublic class Main {
     public static void main(String[] args) {
-
+        String[] array = {"ab", "cd", "ab", "ef", "gh", "cd", "ef", "gh", "ab", "ab"};
+        String total = "";
+        for (int i = 0; i < array.length; i++) {
+            String current = String.valueOf(array[i]);
+            for (int j = 1; j < array.length; j++) {
+                if (i != j && current.equals(array[j])) {
+                    array[j] = null;
+                }
+            }
+            if (array[i] != null) {
+                total += array[i];
+            }
+        }
+        System.out.println(total);
     }
 }
